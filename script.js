dd6eea31dd4582062d7ce83d67cdf2b1515c9201
Ali Mahmoud <961alimahmoud@gmail.com>
2020-10-29 10:08:19 -0400

    fixed local storage

--- script.js
+++ script.js
@@ -8,12 +8,13 @@ for(i=0;i<timeBoxes.length;i++){
     let storageSearch = toString(timeBoxes[i].dataset.time)
 
     if(localStorage.getItem(storageSearch)){
-        timeBoxes[i].children[0].value = localStorage.getItem(storageSearch)
+        timeBoxes[i].children[1].value = localStorage.getItem(storageSearch)
     }
 
     saveButtons[i].addEventListener("click",function(event){
         let content = event.target.previousElementSibling.value;
-        console.log(event.target.parentElement.dataset.time);
+        let label = event.target.parentElement.dataset.time;
+        console.log(label);
         localStorage.setItem(label,content);
     })
     if(timeBoxes[i].dataset.time < currentTime){
@@ -26,3 +27,27 @@ for(i=0;i<timeBoxes.length;i++){
         timeBoxes[i].classList.add("future")
     }
 }
+$("#zero .description").val(localStorage.getItem("0000"))
+$("#one .description").val(localStorage.getItem("0100"))
+$("#two .description").val(localStorage.getItem("0200"))
+$("#three .description").val(localStorage.getItem("0300"))
+$("#four .description").val(localStorage.getItem("0400"))
+$("#five .description").val(localStorage.getItem("0500"))
+$("#six .description").val(localStorage.getItem("0600"))
+$("#seven .description").val(localStorage.getItem("0700"))
+$("#eight .description").val(localStorage.getItem("0800"))
+$("#nine .description").val(localStorage.getItem("0900"))
+$("#ten .description").val(localStorage.getItem("1000"))
+$("#eleven .description").val(localStorage.getItem("1100"))
+$("#twelve .description").val(localStorage.getItem("1200"))
+$("#thirteen .description").val(localStorage.getItem("1300"))
+$("#fourteen .description").val(localStorage.getItem("1400"))
+$("#fifteen .description").val(localStorage.getItem("1500"))
+$("#sixteen .description").val(localStorage.getItem("1600"))
+$("#seventeen .description").val(localStorage.getItem("1700"))
+$("#eighteen .description").val(localStorage.getItem("1800"))
+$("#nineteen .description").val(localStorage.getItem("1900"))
+$("#twenty .description").val(localStorage.getItem("2000"))
+$("#twenty-one .description").val(localStorage.getItem("2100"))
+$("#twenty-two .description").val(localStorage.getItem("2200"))
+$("#twenty-three .description").val(localStorage.getItem("2300"))
